8998579a5b0958db8515526b83eb9db8b351d749
olo126 <141721574+olo126@users.noreply.github.com>
2023-12-06 04:51:57 -0800

    Added transformer code

--- Model.py
+++ Model.py
@@ -56,7 +56,107 @@ class MultiHeadAttention(nn.Module):
         y = torch.permute(y, (0, 2, 1, 3)).reshape(output.shape)
         output = self.proj(y)
         return output
-        
+
+
+class ImageCaptioningTransformer(nn.Module):
+    def __init__(self, word_to_idx, input_dim, wordvec_dim, num_heads=4,
+                 num_layers=2, max_length=50):
+        super().__init__()
+        vocab_size = len(word_to_idx)
+        self.vocab_size = vocab_size
+        self._null = word_to_idx["<NULL>"]
+        self._start = word_to_idx.get("<START>", None)
+        self._end = word_to_idx.get("<END>", None)
+        self.visual_projection = nn.Linear(input_dim, wordvec_dim)
+        self.embedding = nn.Embedding(vocab_size, wordvec_dim, padding_idx=self._null)
+        self.positional_encoding = PositionalEncoding(wordvec_dim, max_len=max_length)
+        decoder_layer = TransformerDecoderLayer(input_dim=wordvec_dim, num_heads=num_heads)
+        self.transformer = TransformerDecoder(decoder_layer, num_layers=num_layers)
+        self.apply(self._init_weights)
+        self.output = nn.Linear(wordvec_dim, vocab_size)
+
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def forward(self, features, captions):
+        N, T = captions.shape
+        scores = torch.empty((N, T, self.vocab_size))
+        embeds = self.positional_encoding(self.embedding(captions))
+        proj_feat = self.visual_projection(features).reshape(N, -1, embeds.shape[-1])
+        tgt_mask = torch.ones(T, T)
+        tgt_mask = torch.tril(tgt_mask)
+        scores = self.transformer(embeds, proj_feat, tgt_mask)
+        scores = self.output(scores)
+        return scores
+
+    def sample(self, features, max_length=30):
+        with torch.no_grad():
+            features = torch.Tensor(features)
+            N = features.shape[0]
+            captions = self._null * np.ones((N, max_length), dtype=np.int32)
+            partial_caption = self._start * np.ones(N, dtype=np.int32)
+            partial_caption = torch.LongTensor(partial_caption)
+            partial_caption = partial_caption.unsqueeze(1)
+            for t in range(max_length):
+                output_logits = self.forward(features, partial_caption)
+                output_logits = output_logits[:, -1, :]
+                word = torch.argmax(output_logits, axis=1)
+                captions[:, t] = word.numpy()
+                word = word.unsqueeze(1)
+                partial_caption = torch.cat([partial_caption, word], dim=1)
+            return captions
+
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(self, input_dim, num_heads, dim_feedforward=2048, dropout=0.1):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(input_dim, num_heads, dropout)
+        self.multihead_attn = MultiHeadAttention(input_dim, num_heads, dropout)
+        self.linear1 = nn.Linear(input_dim, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, input_dim)
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.norm3 = nn.LayerNorm(input_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+        self.activation = nn.ReLU()
+
+
+    def forward(self, tgt, memory, tgt_mask=None):
+        tgt2 = self.self_attn(query=tgt, key=tgt, value=tgt, attn_mask=tgt_mask)
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+        tgt2 = self.multihead_attn(query=tgt, key=memory, value=memory)
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+        return tgt
+
+def clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, decoder_layer, num_layers):
+        super().__init__()
+        self.layers = clones(decoder_layer, num_layers)
+        self.num_layers = num_layers
+
+    def forward(self, tgt, memory, tgt_mask=None):
+        output = tgt
+        for mod in self.layers:
+            output = mod(output, memory, tgt_mask=tgt_mask)
+        return output
+
 
 class ImageCaptioningModel(nn.Module):
     def __init__(self, vocab_size, max_length=5000):
