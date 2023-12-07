67b146e43102fbe01dde9b06f50c58b34d66b4c9
runying <67864475+Yevsquant@users.noreply.github.com>
2023-12-06 18:43:43 -0800

    fixed bugs in datasampling

--- datasampling.py
+++ datasampling.py
@@ -10,7 +10,9 @@ Original file is located at
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+import numpy as np
 import ast
+import re
 
 class GraphImageDataset(Dataset):
     def __init__(self, csv_files, transform=None):
@@ -21,15 +23,17 @@ class GraphImageDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        data_array = ast.literal_eval(self.data.iloc[idx, 0])
-        data_dict = ast.literal_eval(self.data.iloc[idx, 1])
+      x, y = self.data.iloc[idx]
+      x_tensor = torch.tensor(self.str2list(x), dtype=torch.float32)
+      y_out = str(y)
+      return x_tensor, y_out
 
-        # Apply transformations if provided
-        if self.transform:
-            data_array = self.transform(data_array)
-
-        return data_array, data_dict
-
+    def str2list(self, string):
+      processed_string = re.findall(r'\b\d+\b', string)
+      rgb_values = [int(match) for match in processed_string]
+      nested_list = [rgb_values[i:i+3] for i in range(0, len(rgb_values), 3)]
+      return nested_list
+    
 # you might want to change the file names based on your address
 csv_files1 = ['/content/data_kk0.csv', '/content/data_cr0.csv', '/content/data_gv0.csv', '/content/data_sp0.csv']
 csv_files2 = ['/content/data_kk0.csv', '/content/data_cr0.csv', '/content/data_gv0.csv', '/content/data_sp0.csv',
